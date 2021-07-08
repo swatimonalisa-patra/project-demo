a698f273f1a23a91c40c1d1d94d876f14d382de8
swatimonalisa-patra <swatimonalisa8@gmail.com>
2021-07-08 21:53:49 +0530

    added some new files

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//active navbar
+
+$(window).onn('scroll',function(){
+    if ($(window).scrollTop()) {
+        $('nav').addClass('white')
+    }
+    else{
+        $('nav').removeClass('white');
+    }
+})
+
+//nav hide
+
+//counter design
+
